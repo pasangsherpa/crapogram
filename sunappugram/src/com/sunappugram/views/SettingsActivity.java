0246302290361571ef3a9d66ff14fb6737acf13e
nyigilo <Aden@Edward-Bassans-MacBook-Pro.local>
2014-03-16 20:31:11 -0700

    settings

---
+++ sunappugram/src/com/sunappugram/views/SettingsActivity.java
@@ -0,0 +1,18 @@
+package com.sunappugram.views;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.sunappugram.R;
+
+public class SettingsActivity extends Activity {
+	
+	
+	/** Called when the activity is first created. */
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		
+	}
+}
