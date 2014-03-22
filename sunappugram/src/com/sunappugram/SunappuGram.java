25e721d58ce42bd54de23431cc20c685f4ccb79c
InnoIrving Estrera <innoirvinge@gmail.com>
2014-03-22 01:22:50 -0700

    key should be 1, deleted toshi's key

--- sunappugram/src/com/sunappugram/SunappuGram.java
+++ sunappugram/src/com/sunappugram/SunappuGram.java
@@ -12,12 +12,7 @@ public class SunappuGram extends Application {
 		super.onCreate();
 
 		// Add your initialization code here
-<<<<<<< HEAD
-		  Parse.initialize(this, "y6IjnJUmL7AGRiCDld2o6Si8MfyW1mjWDsITYHxh", "UpyQG0fglJLcxRdCVpTZstZco2L4wipGgV6bmsoS");
-
-=======
 		  Parse.initialize(this, "B6JiY65Rydt4lUIhxywPdBlW0st6bFDvhs8a1GDt", "9JGgUGe0WZwpiB9E32qFnsgEWv7RggGPM7XGXQO1");
->>>>>>> 2a2836296349637f94b3ee7ecd6161f869457afc
 
 		ParseUser.enableAutomaticUser();
 
